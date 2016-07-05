eeb1653b5cea5f9de0fd86652225cb35653c8638
Kami Boers <kamiboers@gmail.com>
2016-07-05 01:15:58 -0600

    initial commit

---
+++ js/page.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){ 
+
+	 // $( "div#teal" ).slideDown( "slow", function() {
+  // });
+
+	// $( "div#teal" ).animate({
+ //  		opacity: .4,
+ //  		height: "150%"
+	// 	}, {
+ //  	step: function( now, fx ) {
+ //    var data = fx.elem.id + " " + fx.prop + ": " + now;
+ //    // $( "body" ).append( "<div>" + data + "</div>" );
+	//   }
+	// });
+
+$("div#teal").animate({ "height": "300%" }, 3000 );
+$("div#teal1").animate({ "height": "300%" }, 2800 );
+// $("div#teal2").animate({ "height": "300%" }, 5000 );
+// $("div#teal3").animate({ "height": "300%" }, 3800 );
+
+});
+
