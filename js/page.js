c065bb8f6f976d48ae058231f0cba3481898d45e
Kami Boers <kamiboers@gmail.com>
2016-07-21 16:29:47 -0600

    css animate

--- js/page.js
+++ js/page.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){ 
 
+$('.links').delegate('a', 'click', function() {
+  $('div#' + this.parentElement.id).addClass('fadeInUpBig');
+  // .fadeToggle();
+  // $('.lightdiv').not('#' + this.id).addClass('fadeOutDownBig').fadeOut();
+});
 
 });
 
