5605b350f371159d875ca40efeca4446940d6ac8
Kami Boers <kamiboers@gmail.com>
2016-07-21 17:32:47 -0600

    inner panel display content

--- js/page.js
+++ js/page.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
-// $('.panel').hide();
+$('.panel').hide();
 
 $('.links').delegate('a', 'click', function() {
   $('div#' + this.parentElement.id + '-panel').fadeToggle().toggleClass('fadeInUpBig');
