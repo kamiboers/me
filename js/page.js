72828d5555feceb6e10b24bbf50714a9f733e965
Kami Boers <kamiboers@gmail.com>
2016-07-21 17:09:39 -0600

    clean up opacity and animation

--- js/page.js
+++ js/page.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){ 
+$('.panel').hide();
+$('#side-mask').hide();
 
 $('.links').delegate('a', 'click', function() {
-  $('div#' + this.parentElement.id).addClass('fadeInUpBig');
-  // .fadeToggle();
-  // $('.lightdiv').not('#' + this.id).addClass('fadeOutDownBig').fadeOut();
+  $('div#' + this.parentElement.id + '-panel').fadeToggle().toggleClass('fadeInUpBig');
+  $('.panel').not('#' + this.parentElement.id + '-panel').fadeOut('fast').removeClass('fadeInUpBig');
 });
 
 });
-
