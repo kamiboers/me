0438c997fe06c39309b953f033a69487d08c3dcc
Kami Boers <kamiboers@gmail.com>
2016-07-21 13:19:41 -0600

    a

--- js/page.js
+++ js/page.js
@@ -1,18 +1,6 @@
-$('.lightdiv').hide();
 $(document).ready(function(){ 
 
-	$("div#bar1").animate({ "height": "300%" }, 3000 );
-	$("div#bar2").animate({ "height": "300%" }, 2800 );
-
-$("a.touch").hover(function () {
-    $(this).toggleClass('animated pulse');
-});
-
-
-$('.bar-opaque').delegate('a', 'click', function() {
-	$('div#' + this.id).toggleClass('fadeOutDownBig').fadeToggle();
-	$('.lightdiv').not('#' + this.id).addClass('fadeOutDownBig').fadeOut();
-});
+	$("div#sb-trans").animate({ "height": "100%" }, 3000 );
 
 });
 
