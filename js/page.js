adbaf8c8d2218cd05257606de988b4bbac7bee60
Kami Boers <kamiboers@gmail.com>
2016-07-22 00:41:30 -0600

    style and images

--- js/page.js
+++ js/page.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){ 
 $('.panel').hide();
 
+
 $('.links').delegate('a', 'click', function() {
   $('div#' + this.parentElement.id + '-panel').fadeToggle().toggleClass('fadeInUpBig');
   $('.panel').not('#' + this.parentElement.id + '-panel').fadeOut('fast').removeClass('fadeInUpBig');
 });
 
+  $("#qbert").hover(
+      function() {
+          $(this).attr("src", "images/qbertrox.gif");
+      },
+      function() {
+          $(this).attr("src", "images/qbertstatic.png");
+  });       
+
+  $("#fitbeat").hover(
+      function() {
+          $(this).attr("src", "images/fitbeat.gif");
+      },
+      function() {
+          $(this).attr("src", "images/fitbeatstatic.png");
+  });      
+
+  $("#ideabox").hover(
+      function() {
+          $(this).attr("src", "images/ideabox.gif");
+      },
+      function() {
+          $(this).attr("src", "images/ideaboxstatic.png");
+  });                  
 });
