df435cfe4263c242083df74d12aef1084c62b57e
Kami Boers <kamiboers@gmail.com>
2016-09-04 12:29:37 -0600

    page.js refactor

--- js/page.js
+++ js/page.js
@@ -1,31 +1,14 @@
-// $('.sidebar-content').load("./partials/sidebar.html")
+var panels = ['about','code', 'contact']
+
+for (var i in panels) {
+  $('#panel-' + panels[i]).load('./partials/' + panels[i] + '.html');
+}
 
 $(document).ready(function(){
 
-  $('.sidebar-link').click( function() {
-    console.log('link clicked');
-    if ($('#panel-content').is(':empty')) {      
-      console.log('panel was empty');
-      $('#panel-content').load("./partials/" + this.id + ".html");
-      $('#panel-content').addClass('slideUp');
-
-    } else {
-        console.log('panel was not empty');
-        $('#panel-content').removeClass('slideUp').addClass('slideDown');
-        debugger;
-        if ($('#panel-content:first-child').id === this.id + "-inner-panel") {
-          console.log('closing panel');
-          
-      } else {
-        console.log('switching panel)');
-        $('#panel-content').load("./partials/" + this.id + ".html");
-        $('#panel-content').delay(500).addClass('slideUp');
-      }
-    }
-    
-    // $('#panel-content').delay(500).removeClass('slideDown').addClass('slideUp')
-
-    // $('.panel').not('#' + this.parentElement.id + '-panel').fadeOut('slow').removeClass('fadeInUpBig');
-  });
+    $('.sidebar-link').click(function() {
+      $('#panel-' + this.id).fadeToggle().addClass('slideUp');
+      $('.panel-content').not('#panel-' + this.id ).fadeOut().removeClass('slideUp');
+    });
 
 });
